e1a45cfb3f6c78faee4a131dcb46e55ea5a77698
Till Pilarczyk <till.pilarczyk@study.hs-duesseldorf.de>
2022-09-30 14:23:44 +0200

    init

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header"> <h1>Willkommen zu PDF Utils</h1></div>
+</template>
+
+<script>
+
+</script>
+
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
